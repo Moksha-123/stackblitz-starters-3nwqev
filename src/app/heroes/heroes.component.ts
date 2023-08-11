6d75a1324fde1097bd79a5d427ac82b121639ddf
Moksha-123 <141109376+Moksha-123@users.noreply.github.com>
2023-08-11 11:23:08 +0530

    2 files modified, 1 added

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -2,20 +2,21 @@ import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import {Hero} from '../hero';
+import {HEROES} from '../mok-heroes';
 @Component({
   standalone: true,
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
   styleUrls: ['./heroes.component.css'],
-  imports:[CommonModule,FormsModule]
+  imports:[CommonModule,FormsModule],
 })
 export class HeroesComponent implements OnInit {
   // hero = 'Ranbir Kapoor';
   hero:Hero={
     id:1,
     name:'Ranbir Kapoor',
-  }
-
+}
+heroes = HEROES;
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/mok-heroes.ts
@@ -0,0 +1,9 @@
+import {Hero} from './hero';
+export const HEROES: Hero[] = [
+{id: 45, name: 'Moksha'},
+{id: 55, name: 'Says'},
+{id: 65,name: 'Hello'},
+{id: 75, name: 'to everyone'},
+
+
+];
